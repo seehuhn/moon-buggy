2a50bad6db375d8bc485f2517a309cfff532404f
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1998-12-18 23:17:35 +0000

    Added prototypes for the new files.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@7 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- moon.h
+++ moon.h
@@ -2,10 +2,13 @@
  *
  * Copyright (C) 1998  Jochen Voss.
  *
- * $Id: moon.h,v 1.1 1998/12/17 20:00:29 voss Exp $ */
+ * $Id: moon.h,v 1.2 1998/12/18 23:17:35 voss Exp $ */
 
 #ifndef FILE_MOON_H_SEEN
 #define FILE_MOON_H_SEEN
+
+#include <curses.h>
+WINDOW *status, *moon;
 
 extern  const char *my_name;
 
@@ -24,5 +27,30 @@ extern  void  warning (const char *format, ...)
 extern  void  fatal (const char *format, ...);
 extern  void  warning (const char *format, ...);
 #endif
+
+/* from "queue.c" */
+enum event_type { ev_KEY, ev_SCROLL, ev_BUGGY, ev_SCORE };
+extern  void  add_event (double t, enum event_type type);
+extern  enum event_type  get_event (double *t);
+extern  int  remove_event (enum event_type type, double *t);
+
+/* from "ground.c" */
+extern  chtype *ground1, *ground2;
+extern  void  scroll_ground (void);
+
+/* from "buggy.c" */
+extern  int  car_base;
+extern  void  print_buggy (void);
+extern  void  jump (double t);
+extern  int  can_jump (void);
+extern  int  crash_check (void);
+
+/* from "lag.c" */
+struct lagmeter;
+extern  struct lagmeter *new_lagmeter (void);
+extern  void  add_lag (struct lagmeter *lm, double x);
+extern  double  get_lag (const struct lagmeter *lm);
+
+extern  struct lagmeter *queuelag;
 
 #endif /* FILE_MOON_H_SEEN */
