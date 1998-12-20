81eb333e201acb16f76573c0a69a421b3a491e56
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1998-12-20 00:43:55 +0000

    initial revision
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@15 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

---
+++ mbpaths.c
@@ -0,0 +1,15 @@
+/* mbpaths.c - the central highscore file location
+ *
+ * Copyright (C) 1998  Jochen Voss.  */
+
+static const  char  rcsid[] = "$Id: mbpaths.c,v 1.1 1998/12/20 00:43:55 voss Exp $";
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "moon.h"
+
+
+const char *score_dir = SCORE_DIR;
