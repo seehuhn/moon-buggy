04a6339c8160328162afba8d92cf75bb0eae4a74
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1998-12-20 00:46:54 +0000

    Reflect the fact, that "moon-buggy" is always interactive.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@22 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- error.c
+++ error.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 1998  Jochen Voss.  */
 
-static const  char  rcsid[] = "$Id: error.c,v 1.1 1998/12/17 19:59:31 voss Exp $";
+static const  char  rcsid[] = "$Id: error.c,v 1.2 1998/12/20 00:46:54 voss Exp $";
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,25 +16,16 @@ static const  char  rcsid[] = "$Id: error.c,v 1.1 1998/12/17 19:59:31 voss Exp $
 void
 fatal (const char *format, ...)
 /* Signal a fatal error and quit immediately.  The arguments have the
- * same meaning, as in the function `printf'.  The function tries to
- * conform with the "GNU coding standards". */
+ * same meaning, as in the function `printf'.  */
 {
   va_list  ap;
 
+  prepare_for_exit ();
+  
   va_start (ap, format);
   fflush (NULL);
-  if (isatty (STDIN_FILENO)) {
-    fputs ("Fatal error: ", stderr);
-  } else {
-    fprintf (stderr, "%s: ", my_name);
-  }
-  if (! isatty (STDIN_FILENO) && isupper (format[0])) {
-    /* We all hope, that '%' is no upper-case letter.  */
-    fputc (tolower (format[0]), stderr);
-    vfprintf (stderr, format+1, ap);
-  } else {
-    vfprintf (stderr, format, ap);
-  }
+  fputs ("Fatal error: ", stderr);
+  vfprintf (stderr, format, ap);
   fputc ('\n', stderr);
   va_end (ap);
 
@@ -43,26 +34,15 @@ fatal (const char *format, ...)
 
 void
 warning (const char *format, ...)
-/* Emit a warning message.  The arguments have the
- * same meaning, as in the function `printf'.  The function tries to
- * conform with the "GNU coding standards". */
+/* Emit a warning message.  The arguments have the same meaning, as in
+ * the function `printf'. */
 {
   va_list  ap;
 
   va_start (ap, format);
   fflush (NULL);
-  if (isatty (STDIN_FILENO)) {
-    fputs ("Warning: ", stderr);
-  } else {
-    fprintf (stderr, "%s: ", my_name);
-  }
-  if (! isatty (STDIN_FILENO) && isupper (format[0])) {
-    /* We all hope, that '%' is no upper-case letter.  */
-    fputc (tolower (format[0]), stderr);
-    vfprintf (stderr, format+1, ap);
-  } else {
-    vfprintf (stderr, format, ap);
-  }
+  fputs ("Warning: ", stderr);
+  vfprintf (stderr, format, ap);
   fputc ('\n', stderr);
   va_end (ap);
 }
