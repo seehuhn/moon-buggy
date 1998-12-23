e74cb4cceca073326a3075593de4c5d0da2f5c02
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1998-12-23 09:32:23 +0000

    Changed the type of the global variables `score' and `bonus' to `long'. Added a prototype for `d_rnd'. Removed the prototype for the obsolete function `warning'.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@57 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- moon.h
+++ moon.h
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 1998  Jochen Voss.
  *
- * $Id: moon.h,v 1.4 1998/12/20 13:16:10 voss Exp $ */
+ * $Id: moon.h,v 1.5 1998/12/23 09:32:23 voss Exp $ */
 
 #ifndef FILE_MOON_H_SEEN
 #define FILE_MOON_H_SEEN
@@ -11,11 +11,11 @@
 WINDOW *status, *moon;
 
 extern  const char *my_name;
-extern  unsigned long  score, bonus;
+extern  long  score, bonus;
 
 extern  void  prepare_for_exit (void);
 
-
+extern  int  d_rnd (int limit);
 extern  double  vclock (void);
 extern  void *xmalloc (size_t size);
 extern  void *xrealloc (void *ptr, size_t size);
@@ -25,11 +25,8 @@ extern  char *xstrdup (const char *str);
 #ifdef __GNUC__
 extern  void  fatal (const char *format, ...)
 	__attribute__ ((noreturn)) __attribute__ ((format (printf, 1, 2)));
-extern  void  warning (const char *format, ...)
-	__attribute__ ((format (printf, 1, 2)));
 #else
 extern  void  fatal (const char *format, ...);
-extern  void  warning (const char *format, ...);
 #endif
 
 /* from "queue.c" */
