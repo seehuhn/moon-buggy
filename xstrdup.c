db5e9389e371247ec92a0c04d57ee011b0c406d5
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1998-12-29 00:40:57 +0000

    (xstrndup, xstrnlen): new functions.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@103 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- xstrdup.c
+++ xstrdup.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 1998  Jochen Voss.  */
 
-static const  char  rcsid[] = "$Id: xstrdup.c,v 1.2 1998/12/22 22:29:55 voss Exp $";
+static const  char  rcsid[] = "$Id: xstrdup.c,v 1.3 1998/12/29 00:40:57 voss Rel $";
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -19,5 +19,27 @@ xstrdup (const char *str)
   char *tmp = xmalloc (strlen(str) + 1);
   if (tmp == NULL)  fatal ("Memory exhausted");
   strcpy (tmp, str);
+  return  tmp;
+}
+
+static size_t
+xstrnlen (const char *str, size_t size)
+{
+  size_t  n = 0;
+  while (n<size && str[n])  ++n;
+  return  n;
+}
+
+char *
+xstrndup (const char *str, size_t size)
+/* Like `strndup' from glibc, but never returns NULL.  */
+{
+  size_t  n = xstrnlen (str, size);
+  char *tmp = xmalloc (n + 1);
+  
+  if (tmp == NULL)  fatal ("Memory exhausted");
+  strncpy (tmp, str, n);
+  tmp[n] = '\0';
+  
   return  tmp;
 }
