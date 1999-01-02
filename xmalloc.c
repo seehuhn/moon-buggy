c1244761424f7206bb915e96db0c814879b6a4f5
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1999-01-02 12:41:49 +0000

    Fixed the copyright notice.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@198 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- xmalloc.c
+++ xmalloc.c
@@ -1,24 +1,8 @@
 /* xmalloc.c - memory allocation with error checking
  *
- * Copyright (C) 1998  Jochen Voss
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * Written by Jochen Voﬂ (voss@mathematik.uni-kl.de).  */
+ * Copyright 1998  Jochen Voss  */
 
-static const  char  rcsid[] = "$Id: xmalloc.c,v 1.3 1998/12/29 18:05:29 voss Exp $";
+static const  char  rcsid[] = "$Id: xmalloc.c,v 1.4 1999/01/02 12:41:49 voss Rel $";
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
