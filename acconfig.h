d7d8b41e7c8059d9e83453a5496a7bbae3be3167
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1999-01-30 17:20:53 +0000

    Add HAVE_SYS_SELECT_H and NO_FD_SET.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@257 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- acconfig.h
+++ acconfig.h
@@ -1,3 +1,8 @@
+/* Define if you have the <errno.h> header file.  */
+#undef HAVE_SYS_SELECT_H
+
+/* Define if your system lacks `fd_set' */
+#undef NO_FD_SET
 
 /* The name of the package */
 #undef PACKAGE
