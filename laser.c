3bba80006aebe91be9696b20974d03635ae3602a
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1999-04-25 17:08:31 +0000

    (struct beam): added new `y' field. (beam_handler): clouds move with the ground, now.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@319 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- laser.c
+++ laser.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 1999  Jochen Voss.  */
 
-static const  char  rcsid[] = "$Id: laser.c,v 1.1 1999/04/24 17:48:30 voss Exp $";
+static const  char  rcsid[] = "$Id: laser.c,v 1.2 1999/04/25 17:08:31 voss Exp $";
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -15,7 +15,7 @@ enum beam_state { bs_START, bs_RUN, bs_CLOUD };
 
 struct beam {
   enum beam_state  state;
-  int  left, right;
+  int  left, right, y;
   int  count;
 };
 
@@ -28,32 +28,41 @@ beam_handler (game_time t, void *client_data)
 
   switch (b->state) {
   case bs_START:
-    wmove (moon, LINES-5, b->left);
+    wmove (moon, LINES-b->y, b->left);
     for (i=0; i<=b->right-b->left; ++i)  waddch (moon, '-');
     b->state = bs_RUN;
     b->count = 40;
-    add_event (t+TICK(0.5), beam_handler, client_data);
+    add_event (t+TICK(0.25), beam_handler, client_data);
     break;
   case bs_RUN:
     if (b->count > 0) {
-      mvwaddch (moon, LINES-5, b->right, ' ');
+      mvwaddch (moon, LINES-b->y, b->right, ' ');
       b->left -= 1;
       b->right -= 1;
-      mvwaddch (moon, LINES-5, b->left, '-');
+      mvwaddch (moon, LINES-b->y, b->left, '-');
       b->count -= 1;
-      add_event (t+TICK(0.5), beam_handler, client_data);
+      add_event (t+TICK(0.25), beam_handler, client_data);
     } else {
-      wmove (moon, LINES-5, b->left);
+      wmove (moon, LINES-b->y, b->left);
       for (i=0; i<2; ++i)  waddch (moon, '*');
       for (i=2; i<=b->right-b->left; ++i)  waddch (moon, ' ');
       b->state = bs_CLOUD;
-      add_event (t+TICK(8), beam_handler, client_data);
+      b->count = 3;
+      add_event (t+TICK(1), beam_handler, client_data);
     }
     break;
   case bs_CLOUD:
-    wmove (moon, LINES-5, b->left);
-    for (i=0; i<2; ++i)  waddch (moon, ' ');
-    free (b);
+    if (b->count > 0) {
+      mvwaddch (moon, LINES-b->y, b->left, ' ');
+      b->left += 1;
+      mvwaddch (moon, LINES-b->y, b->left+1, '*');
+      b->count -= 1;
+      add_event (t+TICK(1), beam_handler, client_data);
+    } else {
+      wmove (moon, LINES-b->y, b->left);
+      for (i=0; i<2; ++i)  waddch (moon, ' ');
+      free (b);
+    }
     break;
   }
 }
@@ -63,7 +72,8 @@ fire_laser (double t)
 {
   struct beam *b = xmalloc (sizeof (struct beam));
   b->state = bs_START;
-  b->left = car_base-speed-8;
-  b->right = car_base-speed-1;
-  add_event (t+TICK(0.5), beam_handler, b);
+  b->left = car_x-8;
+  b->right = car_x-1;
+  b->y = car_y;
+  add_event (t+TICK(0.25), beam_handler, b);
 }
