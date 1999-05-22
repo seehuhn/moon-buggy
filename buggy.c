9f654716fb20c459bb82e0502dae2a501e310b78
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1999-05-22 11:57:06 +0000

    (struct_scene): complete change Removed the obsolete variable `cstate'. Joined old `animate_buggy' into `print_buggy'. (print_buggy): use the table from "buggy.h".  Do not check for crashs. (jump, can_jump, crash_check): fixes.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@362 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- buggy.c
+++ buggy.c
@@ -2,7 +2,7 @@
  *
  * Copyright 1999  Jochen Voss  */
 
-static const  char  rcsid[] = "$Id: buggy.c,v 1.11 1999/04/25 17:10:32 voss Exp $";
+static const  char  rcsid[] = "$Id: buggy.c,v 1.12 1999/05/22 11:57:06 voss Exp $";
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -18,143 +18,100 @@ int  car_x, car_y;
 
 
 typedef  struct scene {
-  double  t;
-  enum car_state  state;
+  enum car_state  n;		/* image number (index to array `image') */
+  int  y;			/* vertical position */
+  double  dt;			/* time to next state */
+  int  has_ground;		/* flag, true iff we may crash or jump */
 }  scenario [];
 
 static  scenario  sz_empty = {
-  { -1, -1 }
+  { car_NORMAL, 5, -1, 1 }
 };
 
-static  scenario  sz_jump = {
-  { TICK(0), car_START },
-  { TICK(1.25), car_UP1 },
-  { TICK(3.75), car_UP2 },
-  { TICK(8.75), car_UP1 },
-  { TICK(11.25), car_LAND },
-  { TICK(13.75), car_NORMAL },
-  { -1, -1 }
+static scenario sz_jump = {
+  { car_START, 5, TICK(1.25), 0 },
+  { car_UP1, 6, TICK(2.5), 0 },
+  { car_UP2, 7, TICK(5), 0 },
+  { car_UP1, 6, TICK(2.5), 0 },
+  { car_LAND, 5, TICK(2.5), 1 },
+  { car_NORMAL, 5, -1, 1 }
 };
 
 static  struct scene *state;
-static  enum car_state  cstate;
+
 
 void
 initialise_buggy (void)
 {
   state = sz_empty;
-  cstate = car_NORMAL;
 }
 
-static void
-animate_buggy (void)
-{
-  if (state->t >= -0.5) {
-    cstate = state->state;
-    ++state;
-  }
-}
-
-int
+void
 print_buggy (void)
 {
-  int  crash = 0;
-  
-  switch (cstate) {
-  case car_NORMAL:
-    car_y = 5;
-    mvwaddstr (moon, LINES-8, car_x, "         ");
-    mvwaddstr (moon, LINES-7, car_x, "         ");
-    mvwaddstr (moon, LINES-6, car_x, "    Omm  ");
-    mvwaddstr (moon, LINES-5, car_x, " (0)-(0) ");
-    break;
-  case car_START:
-    car_y = 5;
-    mvwaddstr (moon, LINES-8, car_x, "         ");
-    mvwaddstr (moon, LINES-7, car_x, "         ");
-    mvwaddstr (moon, LINES-6, car_x, "    OMM  ");
-    mvwaddstr (moon, LINES-5, car_x, " (0)-(0) ");
-    break;
-  case car_UP1:
-    car_y = 6;
-    mvwaddstr (moon, LINES-8, car_x, "         ");
-    mvwaddstr (moon, LINES-7, car_x, "    OMm  ");
-    mvwaddstr (moon, LINES-6, car_x, " (0)-(0) ");
-    mvwaddstr (moon, LINES-5, car_x, "         ");
-    break;
-  case car_UP2:
-    car_y = 7;
-    mvwaddstr (moon, LINES-8, car_x, "    oMm  ");
-    mvwaddstr (moon, LINES-7, car_x, " (0)-(0) ");
-    mvwaddstr (moon, LINES-6, car_x, "         ");
-    mvwaddstr (moon, LINES-5, car_x, "         ");
-    break;
-  case car_LAND:
-    crash = crash_check ();
-    if (! crash) {
-      car_y = 5;
-      mvwaddstr (moon, LINES-8, car_x, "         ");
-      mvwaddstr (moon, LINES-7, car_x, "         ");
-      mvwaddstr (moon, LINES-6, car_x, "    omm  ");
-      mvwaddstr (moon, LINES-5, car_x, " (o)_(o) ");
-    }
-    break;
-  }
+  enum car_state  n = state->n;
+  int  y = state->y;
 
+  if (y < car_y) {
+    mvwaddstr (moon, LINES-car_y-1, car_x, "       ");
+  } else if (y > car_y) {
+    mvwaddstr (moon, LINES-car_y, car_x, "       ");
+  }
+  car_y = y;
+  mvwaddstr (moon, LINES-y-1, car_x, image[n][0]);
+  mvwaddstr (moon, LINES-y, car_x, image[n][1]);
   wnoutrefresh (moon);
-  return  crash;
 }
 
 static void
 jump_handler (game_time t, void *client_data)
 {
-  animate_buggy ();
-  if (print_buggy ())  quit_main_loop ();
+  state = client_data;
+  print_buggy ();
+  if (crash_check ())  quit_main_loop ();
+  if (state->dt >= -0.5) {
+    add_event (t+state->dt, jump_handler, state+1);
+  }
 }
 
 void
 jump (double t)
 {
-  struct scene *sptr = sz_jump;
-
-  assert (cstate == car_NORMAL || cstate == car_LAND);
-  remove_event (jump_handler);
-
-  cstate = car_START;
-  state = sptr;
-  while (sptr->t >= -0.5) {
-    add_event (t+sptr->t, jump_handler, NULL);
-    ++sptr;
-  }
+  assert (state->has_ground);
+  remove_event (jump_handler);	/* only on jump at a time */
+  add_event (t, jump_handler, sz_jump);
 }
 
 int
 can_jump (void)
 {
-  return  (cstate == car_NORMAL || cstate == car_LAND);
+  return  state->has_ground;
 }
 
 int
 crash_check (void)
+/* Return true, if the car crashed.  */
 {
-  int  base = car_x;
-  
-  if (cstate == car_START || cstate == car_UP1 || cstate == car_UP2)  return 0;
-  if (ground2[base+2] == ' ') {
-    mvwaddstr (moon, LINES-8, base, "         ");
-    mvwaddstr (moon, LINES-7, base, "  m      ");
-    mvwaddstr (moon, LINES-6, base, "  Om     ");
-    mvwaddstr (moon, LINES-5, base, " (;-(*)  ");
-    mvwaddch (moon, LINES-4, base+2,  'o');
+  if (! state->has_ground)  return 0;
+  if (ground2[car_x+1] == ' ') {
+    mvwaddstr (moon, LINES-car_y-1, car_x, "       ");
+    mvwaddstr (moon, LINES-car_y, car_x, "       ");
+    mvwaddstr (moon, LINES-8, car_x, "       ");
+    mvwaddstr (moon, LINES-7, car_x, " m     ");
+    mvwaddstr (moon, LINES-6, car_x, " Om    ");
+    mvwaddstr (moon, LINES-5, car_x, "(;-(*) ");
+    mvwaddch (moon, LINES-4, car_x+1, 'o');
     wnoutrefresh (moon);
     return 1;
   }
-  if (ground2[base+6] == ' ') {
-    mvwaddstr (moon, LINES-8, base, "         ");
-    mvwaddstr (moon, LINES-7, base, "         ");
-    mvwaddstr (moon, LINES-6, base, "     0   ");
-    mvwaddstr (moon, LINES-5, base, " (*;_(m. ");
-    mvwaddch (moon, LINES-4, base+6,      'o');
+  if (ground2[car_x+5] == ' ') {
+    mvwaddstr (moon, LINES-car_y-1, car_x, "       ");
+    mvwaddstr (moon, LINES-car_y, car_x, "       ");
+    mvwaddstr (moon, LINES-8, car_x, "       ");
+    mvwaddstr (moon, LINES-7, car_x, "       ");
+    mvwaddstr (moon, LINES-6, car_x, "    0  ");
+    mvwaddstr (moon, LINES-5, car_x, "(*;_(m.");
+    mvwaddch  (moon, LINES-4, car_x+5,    'o');
     wnoutrefresh (moon);
     return 1;
   }
@@ -164,8 +121,8 @@ crash_check (void)
   }
 
 #if MB_DEBUG
-  ground2[base+2] = 'O';
-  ground2[base+6] = 'O';
+  ground2[car_x+1] = 'O';
+  ground2[car_x+5] = 'O';
 #endif
   
   return  0;
