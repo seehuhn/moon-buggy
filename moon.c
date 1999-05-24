c98f7ae2b39fbf4a9c72faa0bf6b863497bea606
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1999-05-24 19:13:28 +0000

    New score system. (bonus): new global variable. (print_level): new function
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@415 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- moon.c
+++ moon.c
@@ -2,7 +2,7 @@
  *
  * Copyright 1999  Jochen Voss  */
 
-static const  char  rcsid[] = "$Id: moon.c,v 1.19 1999/05/23 20:59:39 voss Exp $";
+static const  char  rcsid[] = "$Id: moon.c,v 1.20 1999/05/24 19:13:28 voss Rel $";
 
 
 #ifdef HAVE_CONFIG_H
@@ -18,11 +18,11 @@ static const  char  rcsid[] = "$Id: moon.c,v 1.19 1999/05/23 20:59:39 voss Exp $
 #  define memmove(d, s, n) bcopy ((s), (d), (n))
 # endif
 #endif
-#include <assert.h>
 
 #include "mbuggy.h"
 
 
+int *bonus;			/* points to get, if we drive over them */
 char *ground1, *ground2;
 static int  ground_width;
 
@@ -35,10 +35,12 @@ resize_ground (int clear_it)
   block_winch ();
   cols = COLS;
   if (ground_width != cols) {
+    bonus = xrealloc (bonus, cols*sizeof(int));
     ground1 = xrealloc (ground1, cols);
     ground2 = xrealloc (ground2, cols);
   }
   for (i=(clear_it ? 0 : ground_width); i<cols; ++i) {
+    bonus[i] = 0;
     ground1[i] = '#';
     ground2[i] = '#';
   }
@@ -56,19 +58,32 @@ print_ground (void)
 }
 
 static void
+print_level (void)
+{
+  mvwprintw (status, 0, car_base-32, "level: %d", current_level () + 1);
+  wnoutrefresh (status);
+}
+
+static void
 scroll_handler (game_time t, void *client_data)
 {
-  char  nextchar = level_tick (t) ? ' ' : '#';
-  
+  memmove (bonus+1, bonus, (ground_width-1)*sizeof(int));
   memmove (ground2+1, ground2, ground_width-1);
-  ground2[0] = nextchar;
+  level_tick (t);
   print_ground ();
+  print_level ();
 
-  if (ground2[car_x + 7] == ' ')  ++bonus;
+  stakes += bonus[car_x + 7];
+  
   if (crash_detected)  shift_buggy (1);
   if (crash_detected || crash_check ()) {
     ++crash_detected;
     if (crash_detected > 2)  quit_main_loop ();
+  }
+
+  if (can_jump () && stakes) {
+    adjust_score (stakes);
+    stakes = 0;
   }
   
   add_event (t+TICK(1), scroll_handler, NULL);
