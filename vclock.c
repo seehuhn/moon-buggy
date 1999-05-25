30c28d4baa0dabf2b6c671e2bab843aa60dc6228
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1999-05-25 15:26:06 +0000

    Define _XOPEN_SOURCE_EXTENDED.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@430 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- vclock.c
+++ vclock.c
@@ -2,12 +2,13 @@
  *
  * Copyright (C) 1998  Jochen Voss.  */
 
-static const  char  rcsid[] = "$Id: vclock.c,v 1.2 1999/05/22 13:43:59 voss Rel $";
+static const  char  rcsid[] = "$Id: vclock.c,v 1.3 1999/05/25 15:26:06 voss Exp $";
 
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+#define _XOPEN_SOURCE_EXTENDED 1
 
 #include <sys/time.h>
 
