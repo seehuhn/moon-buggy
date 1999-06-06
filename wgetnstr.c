84dc96c7278f3d9772f91f261c1cd2ae10a87110
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
1999-06-06 13:17:19 +0000

    Do not use `xgetch' any more.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@505 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- wgetnstr.c
+++ wgetnstr.c
@@ -2,13 +2,18 @@
  *
  * Copyright 1999  Jochen Voss  */
 
-static const  char  rcsid[] = "$Id: wgetnstr.c,v 1.3 1999/05/25 15:24:50 voss Exp $";
+static const  char  rcsid[] = "$Id: wgetnstr.c,v 1.4 1999/06/06 13:17:19 voss Exp $";
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
 
 #include <ctype.h>
+#if HAVE_ERRNO_H
+#include <errno.h>
+#else
+extern  int  errno;
+#endif
 
 #include "mbuggy.h"
 
@@ -26,12 +31,16 @@ wgetnstr (WINDOW *win, char *str, int n)
   do {
     int  c;
 
-    c = xgetch (win);
+    do {
+      c = wgetch (win);
+    } while (c == ERR && errno == EINTR);
     switch (c) {
     case '\n':
     case '\r':
       done = 1;
       break;
+    case ERR:
+      fatal ("cannot read keyboard input");
     default:
       if (c == erase && pos > 0) {
 	  --pos;
