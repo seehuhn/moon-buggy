c910e4856acd74c56df5022cc39542eac8c552ba
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
2000-04-26 20:43:35 +0000

    removed the maint-buggy script
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@975 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,7 @@
 #! /bin/sh
 # autogen.sh - recreate automatically generated files
-# $Id: autogen.sh,v 1.6 1999/06/25 23:26:06 voss Rel $
+# $Id: autogen.sh,v 1.7 2000/04/26 20:43:35 voss Rel $
 aclocal
 autoconf
 autoheader
-touch uninst.pre.in
 automake -a
-echo "warning: after configure run, remove empty \"uninst.pre.in\""
