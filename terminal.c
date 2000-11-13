a6b1bf6abe55265fcf48022d4b73caa7ecd3f043
voss <voss@267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e>
2000-11-13 20:49:16 +0000

    Renamed the function `mesg_off' to `term_prepare' and `mesg_restore' to `term_restore'. Add code, to disable START and STOP characters.
    git-svn-id: file:///home/voss/svn/svnrepo/trunk@1247 267bc8fb-22bb-0310-b6ae-e91cd1fe7d5e

--- terminal.c
+++ terminal.c
@@ -1,8 +1,8 @@
-/* terminal.c - restrict write access to the terminal
+/* terminal.c - prepare the terminal
  *
  * Copyright (C) 2000  Jochen Voss.  */
 
-static const  char  rcsid[] = "$Id: terminal.c,v 1.1 2000/11/13 20:14:20 voss Exp $";
+static const  char  rcsid[] = "$Id: terminal.c,v 1.2 2000/11/13 20:49:16 voss Exp $";
 
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
@@ -20,48 +20,88 @@ static const  char  rcsid[] = "$Id: terminal.c,v 1.1 2000/11/13 20:14:20 voss Ex
 #else
 extern  int  errno;
 #endif
+#if HAVE_TERMIOS_H
+#include <termios.h>
+#endif
 #include <assert.h>
 
 #include "moon-buggy.h"
 
 
-static  mode_t  term_mode;
-static  int  term_mode_valid;
+static  mode_t  term_mode;	/* The terminal mode before we changed it */
+static  int  term_mode_valid;	/* true iff the above is valid */
+static  int  attributes_changed; /* true iff we changed the IXON attribute */
 
 
 void
-mesg_off (void)
-/* Restrict write access to the terminal as "mesg n" would do.  */
+term_prepare (int mesg_n_flag)
+/* Prepare the terminal for game play.
+ * This tries to disable the C-s key and, if MESG_N_FLAG is set,
+ * restricts write access to the terminal as "mesg n" would do.  */
 {
-  struct stat st;
+  struct stat  st;
+#if HAVE_TERMIOS_H
+  struct termios  settings;
+#endif
   int  res;
   
   assert (! term_mode_valid);
-
   if ( ! isatty (0) )  return;
-  res = fstat (0, &st);
-  if (res < 0)  fatal ("Cannot get terminal attributes: %s", strerror (errno));
 
-  term_mode = st.st_mode;
-  term_mode_valid = 1;
+  if (mesg_n_flag) {
+    res = fstat (0, &st);
+    if (res < 0)  fatal ("Cannot get terminal attributes: %s", strerror (errno));
+    
+    term_mode = st.st_mode;
+    term_mode_valid = 1;
+    
+    st.st_mode &= ~(S_IWGRP|S_IWOTH);
+    res = fchmod (0, st.st_mode);
+    if (res < 0)
+      fatal ("Cannot set terminal permissions: %s", strerror (errno));
+  }
 
-  st.st_mode &= ~(S_IWGRP|S_IWOTH);
-  res = fchmod (0, st.st_mode);
+#if HAVE_TERMIOS_H
+  res = tcgetattr (0, &settings);
   if (res < 0)
-    fatal ("Cannot set terminal permissions: %s", strerror (errno));
+    fatal ("Cannot get terminal attributes: %s", strerror (errno));
+
+  if (settings.c_iflag & IXON) {
+    settings.c_iflag &= ~IXON;
+    res = tcsetattr (0, TCSANOW, &settings);
+    if (res < 0)
+      fatal ("Cannot set terminal attributes: %s", strerror (errno));
+    attributes_changed = 1;
+  }
+#endif
 }
 
 void
-mesg_restore (void)
+term_restore (void)
 /* Restore the terminal's original access mode.  */
 {
+#if HAVE_TERMIOS_H
+  struct termios  settings;
+#endif
   int  res;
   
-  if (! term_mode_valid)  return;
+#if HAVE_TERMIOS_H
+  if (attributes_changed) {
+    res = tcgetattr (0, &settings);
+    if (res < 0)
+      fatal ("Cannot get terminal attributes: %s", strerror (errno));
+    
+    settings.c_iflag |= IXON;
+    res = tcsetattr (0, TCSANOW, &settings);
+    if (res < 0)
+      fatal ("Cannot set terminal attributes: %s", strerror (errno));
+  }
+#endif
 
-  res = fchmod (0, term_mode);
-  if (res < 0)
-    fatal ("Cannot restore terminal permissions: %s", strerror (errno));
-  
-  term_mode_valid = 0;
+  if (term_mode_valid) {
+    res = fchmod (0, term_mode);
+    if (res < 0)
+      fatal ("Cannot restore terminal permissions: %s", strerror (errno));
+    term_mode_valid = 0;
+  }
 }
